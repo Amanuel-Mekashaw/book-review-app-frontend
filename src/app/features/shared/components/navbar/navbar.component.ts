485afcf866f14089c69de8c7f82bc5d97ab32fe9
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-11-29 16:29:00 +0300

    navbar displays user information logs out and mobile responsive

--- src/app/features/shared/components/navbar/navbar.component.ts
+++ src/app/features/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../Auth/auth.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  isOpen = signal(false);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.authService.currentUserSignal.set(
+      JSON.parse(atob(localStorage.getItem('user'))),
+    );
+  }
+
+  navbarOpen() {
+    this.isOpen.set(!this.isOpen());
+  }
+
+  logout() {
+    this.router.navigateByUrl('/login');
+    this.authService.currentUserSignal.set(null);
+    localStorage.removeItem('token');
+  }
+}
