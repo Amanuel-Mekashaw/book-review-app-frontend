7a040e6545406ed8d60a45ccb5de20533a8b6a26
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-11-30 12:29:53 +0300

    Genre loadig works

---
+++ src/app/features/Genre/Genre.component.ts
@@ -0,0 +1,46 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { Genre } from '../../book_interface';
+import { HttpClient } from '@angular/common/http';
+import { GenreApiResponse } from './genre_interface';
+import { ApiError } from '../shared/book_interface';
+import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from '../shared/components/loading-spinner/loading-spinner.component';
+import { URL } from '../shared/constants';
+
+@Component({
+  selector: 'app-genre',
+  standalone: true,
+  imports: [CommonModule, LoadingSpinnerComponent],
+  templateUrl: './Genre.component.html',
+  styleUrl: './Genre.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GenreComponent {
+  http = inject(HttpClient);
+
+  genres = signal<Genre[] | null | undefined>(null);
+  error = signal('');
+  loading = signal(true);
+
+  isReadMe = signal(false);
+
+  ngOnInit(): void {
+    this.http.get<GenreApiResponse>(`${URL}/genre`).subscribe({
+      next: (response: GenreApiResponse) => {
+        console.log('response', response);
+        this.genres.set(response.content);
+        this.loading.set(false);
+      },
+      error: (error: ApiError) => {
+        console.log('error', error);
+        this.error.set(error.message);
+        this.loading.set(false);
+      },
+    });
+  }
+}
---
+++ src/app/features/Genre/genre_interface.ts
@@ -0,0 +1,30 @@
+import { Genre } from '../../book_interface';
+
+export interface GenreApiResponse {
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  content: Genre[];
+  number: number;
+  sort: SortInfo;
+  numberOfElements: number;
+  pageable: Pageable;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+export interface SortInfo {
+  empty: boolean;
+  unsorted: boolean;
+  sorted: boolean;
+}
+
+export interface Pageable {
+  offset: number;
+  sort: SortInfo;
+  pageSize: number;
+  pageNumber: number;
+  paged: boolean;
+  unpaged: boolean;
+}
