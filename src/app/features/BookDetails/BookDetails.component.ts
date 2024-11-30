9173345429a6b04958ca9446d02536b94db6008a
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-11-30 12:29:41 +0300

    correctly created genre section and improved loading and error display

--- src/app/features/BookDetails/BookDetails.component.ts
+++ src/app/features/BookDetails/BookDetails.component.ts
@@ -9,7 +9,7 @@ import {
 import { ActivatedRoute } from '@angular/router';
 import { URL } from '../shared/constants';
 import { Book } from '../../book_interface';
-import { SingleBookApiError } from '../shared/book_interface';
+import { ApiError } from '../shared/book_interface';
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from '../shared/components/loading-spinner/loading-spinner.component';
 
@@ -25,10 +25,12 @@ export class BookDetailsComponent implements OnInit {
   activeRoute = inject(ActivatedRoute);
   http = inject(HttpClient);
 
-  book = signal<Book>(null);
+  book = signal<Book | null | undefined>(null);
   error = signal('');
   loading = signal(true);
   bookId = signal(this.activeRoute.snapshot.params['id']);
+
+  isReadMe = signal(false);
 
   ngOnInit(): void {
     console.log('book id', this.bookId);
@@ -39,11 +41,15 @@ export class BookDetailsComponent implements OnInit {
         this.book.set(response);
         this.loading.set(false);
       },
-      error: (error: SingleBookApiError) => {
+      error: (error: ApiError) => {
         console.log('error', error);
         this.error.set(error.message);
         this.loading.set(false);
       },
     });
+  }
+
+  openReadMe() {
+    this.isReadMe.update((current) => !current);
   }
 }
