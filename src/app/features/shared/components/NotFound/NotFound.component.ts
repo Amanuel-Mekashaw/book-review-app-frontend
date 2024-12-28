318c87cd03c42dab5e05459e2062ac93e3b4de10
Amanuel Mekashaw <amanuelmekashaw29@gmail.com>
2024-12-28 11:30:43 +0300

    minor tweaks

--- src/app/features/shared/components/NotFound/NotFound.component.ts
+++ src/app/features/shared/components/NotFound/NotFound.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './NotFound.component.html',
   styleUrl: './NotFound.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundComponent { }
+export class NotFoundComponent {}
